233fb49ab3ef2f884f541c201b5dcf10d11a7ad6
Bret Comnes <bcomnes@gmail.com>
2015-07-12 21:15:55 -0700

    Added header and body auth getters

---
+++ test/authBody.js
@@ -0,0 +1,113 @@
+var test = require('tape')
+var request = require('request')
+var http = require('http')
+var portfinder = require('portfinder')
+
+var authBody = require('../lib/authBody')
+var fakeBearer = require('../lib/fakeBearer')
+
+var port
+var server
+
+test('start authBody server', function (t) {
+  portfinder.getPort(function (err, freePort) {
+    if (err) return console.error(err)
+    port = freePort
+    server = http.createServer()
+    server.listen(port, function () {
+      t.pass('server started on ' + port)
+      t.end()
+    })
+  })
+})
+
+test('get bearer token from request body', function (t) {
+  var testToken = fakeBearer()
+  var authReq = {
+    method: 'POST',
+    uri: 'http://localhost:' + port,
+    form: {
+      key: 'value',
+      access_token: testToken
+    }
+  }
+
+  var handler = function (req, res) {
+    console.log(req.headers)
+    authBody(req, function (err, token) {
+      t.error(err, 'no errors when finding body token')
+      t.equal(token, testToken, 'should get bearer token from header')
+      res.statusCode = 200
+      res.end()
+    })
+  }
+
+  server.on('request', handler)
+
+  request(authReq, function (err, res, body) {
+    t.error(err, 'no err on request')
+    server.removeListener('request', handler)
+    t.end()
+  })
+
+})
+
+test('do not return auth header when missing', function (t) {
+  var authReq = {
+    method: 'POST',
+    uri: 'http://localhost:' + port,
+    form: {
+      key: 'value'
+    }
+  }
+
+  var handler = function (req, res) {
+    authBody(req, function (err, token) {
+      t.error(err, 'no errors when finding body token')
+      t.equal(token, undefined, 'should not return a bearer token')
+      res.statusCode = 400
+      res.end()
+    })
+  }
+
+  server.on('request', handler)
+
+  request(authReq, function (err, res, body) {
+    t.equal(res.statusCode, 400, 'missing status code returns 400')
+    t.error(err, 'no err on request')
+    server.removeListener('request', handler)
+    t.end()
+  })
+})
+
+test('do not return auth header when not urlencoded', function (t) {
+  var authReq = {
+    method: 'POST',
+    uri: 'http://localhost:' + port
+  }
+
+  var handler = function (req, res) {
+    authBody(req, function (err, token) {
+      t.error(err, 'body token parsed without error')
+      t.equal(token, undefined, 'should not return a bearer token')
+      res.statusCode = 400
+      res.end()
+    })
+  }
+
+  server.on('request', handler)
+
+  request(authReq, function (err, res, body) {
+    t.equal(res.statusCode, 400, 'missing status code returns 400')
+    t.error(err, 'no err on request')
+    server.removeListener('request', handler)
+    t.end()
+  })
+})
+
+test('stop authHeader server', function (t) {
+  server.close(function () {
+    t.pass('server stopped')
+    t.end()
+  })
+})
--- test/authHeader.js
+++ test/authHeader.js
@@ -4,7 +4,7 @@ var http = require('http')
 var portfinder = require('portfinder')
 var crypto = require('crypto')
 
-var bearer = require('../lib/authHeader')
+var authHeader = require('../lib/authHeader')
 
 var port
 var server
@@ -13,7 +13,7 @@ function fakeBearer () {
   return crypto.randomBytes(64).toString('base64')
 }
 
-test('start a server', function (t) {
+test('start authHeader server', function (t) {
   portfinder.getPort(function (err, freePort) {
     if (err) return console.error(err)
     port = freePort
@@ -36,7 +36,7 @@ test('get auth header from request', function (t) {
   }
 
   var handler = function (req, res) {
-    var token = bearer(req)
+    var token = authHeader(req)
     t.equal(token, testToken, 'should get bearer token from header')
     res.statusCode = 200
     res.end()
@@ -52,7 +52,30 @@ test('get auth header from request', function (t) {
 
 })
 
-test('stop the server', function (t) {
+test('do not return auth header when missing', function (t) {
+  var authReq = {
+    method: 'POST',
+    uri: 'http://localhost:' + port
+  }
+
+  var handler = function (req, res) {
+    var token = authHeader(req)
+    t.equal(token, undefined, 'should not return a bearer token')
+    res.statusCode = 400
+    res.end()
+  }
+
+  server.on('request', handler)
+
+  request(authReq, function (err, res, body) {
+    t.equal(res.statusCode, 400, 'missing status code returns 400')
+    t.error(err, 'no err on request')
+    server.removeListener('request', handler)
+    t.end()
+  })
+})
+
+test('stop authHeader server', function (t) {
   server.close(function () {
     t.pass('server stopped')
     t.end()
