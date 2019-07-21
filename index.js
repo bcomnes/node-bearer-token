afb831aa840921e0b9714b9009e90fc118538a22
Bret Comnes <bcomnes@gmail.com>
2019-07-21 11:46:06 +0200

    Updataes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var nextTick = require('process-nextick-args')
+var nta = require('process-nextick-args')
 
 var authBody = require('./lib/authBody')
 var authHeader = require('./lib/authHeader')
 
 function bearer (req, cb) {
   var headerToken = authHeader(req)
-  if (headerToken) return nextTick(cb, null, headerToken)
+  if (headerToken) return nta.nextTick(cb, null, headerToken)
   else authBody(req, cb)
 }
 
